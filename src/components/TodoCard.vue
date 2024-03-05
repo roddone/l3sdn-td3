36ecc196fffefaec1a0b7a54c4a4bb7d556ff662
Zaouich123 <etienne.baillie@gmail.com>
2024-03-05 16:22:57 +0100

    Card store begins with const for having all things from a todo

--- src/components/TodoCard.vue
+++ src/components/TodoCard.vue
@@ -1,25 +1,25 @@
 <template>
- <q-card class="my-card">
-      <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
+  <q-card class="my-card">
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+      <div class="text-subtitle2">by John Doe</div>
+    </q-card-section>
 
-      <q-separator />
+    <q-separator />
 
     <div class="q-pa-md">
-        <q-checkbox v-model="value" />
+      <q-checkbox v-model="value" />
     </div>
-    </q-card>
+  </q-card>
 </template>
 
-
 <script setup>
-
 import { ref } from 'vue'
+import { useTodoStore } from '@/stores/todostore.js'
+
+const store = useTodoStore()
+
+const todo = store.getTodo(id)
 
 const value = ref(false)
-
-
-
 </script>
