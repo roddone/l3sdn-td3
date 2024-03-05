b23ded75ffc7c9141bb62524bea3d888bbbd533d
Lucas Maveau <lucas.maveau@gmail.com>
2024-03-05 17:39:54 +0100

    ajout date pas obligatoire

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="flex justify-between items-center p-2 bg-gray-100 rounded-lg">
-    <div :class="{ 'line-through': todo.completed }">
-      {{ todo.text }}
+    <div>
+      <div :class="{ 'line-through': todo.completed }">
+        {{ todo.text }}
+      </div>
+      <div class="text-xs text-gray-600">
+        Créé le: {{ todo.created }}
+        <span v-if="todo.due">| À faire avant: {{ todo.due }}</span>
+      </div>
     </div>
     <div>
       <q-checkbox
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="flex flex-col space-y-2">
     <div class="flex">
-      <q-input v-model="newTodo" filled placeholder="Ajouter une nouvelle tâche" class="flex-grow" />
+      <q-input
+        v-model="newTodoText"
+        filled
+        placeholder="Ajouter une nouvelle tâche"
+        class="flex-grow"
+      />
+      <q-input
+        v-model="newTodoDueDate"
+        filled
+        type="date"
+        placeholder="Date limite (optionnel)"
+        class="q-ml-md"
+      />
       <q-btn icon="add" class="q-ml-md" @click="addTodo" />
     </div>
     <div>
-      <TodoItem v-for="(todo, index) in todos" :key="index" :todo="todo" @remove="removeTodo(index)" @toggle="toggleTodoCompletion(index)" />
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+        @remove="removeTodo(index)"
+        @toggle="toggleTodoCompletion(index)"
+        @update:todo="updateTodo(index, $event)"
+      />
     </div>
   </div>
 </template>
@@ -15,23 +34,32 @@ import { ref } from 'vue'
 import TodoItem from './TodoItem.vue'
 
 const todos = ref([])
-const newTodo = ref('')
+const newTodoText = ref('')
+const newTodoDueDate = ref('')
 
 function addTodo() {
-  if (newTodo.value.trim()) {
-    todos.value.push({ text: newTodo.value, completed: false })
-    newTodo.value = ''
+  if (newTodoText.value.trim()) {
+    todos.value.push({
+      text: newTodoText.value,
+      completed: false,
+      created: new Date().toISOString().substring(0, 10),
+      due: newTodoDueDate.value || null
+    })
+    newTodoText.value = ''
+    newTodoDueDate.value = ''
   }
 }
 
 function removeTodo(index) {
   todos.value.splice(index, 1)
 }
-function updateTodo(index, newTodo) {
-  todos.value[index] = newTodo
-}
+
 function toggleTodoCompletion(index) {
   const todo = todos.value[index]
   todo.completed = !todo.completed
+}
+
+function updateTodo(index, updatedTodo) {
+  todos.value[index] = updatedTodo
 }
 </script>
