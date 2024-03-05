ce2b301f6244d3b5b19e95ddd18c03c7c29c8cd5
Alyson Okinda <alysonokinda@gmail.com>
2024-03-05 16:11:33 +0100

    store setup

---
+++ src/stores/todoStore.js
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+
+export const useTodoStore = defineStore('todos', {
+  state: () => ({
+    todoList: [
+      {
+        id: 1,
+        description: 'Learn Vue',
+        state: 'Done',
+        date: '2022-01-01',
+      },
+      {
+        id: 2,
+        description: 'Learn Quasar',
+        state: 'Done',
+        date: '2022-01-01',
+      },
+      {
+        id: 3,
+        description: 'Learn Js',
+        state: 'Done',
+        date: '2022-01-01',
+      }
+    ]
+  }),
+  actions: {
+    getTodoList() {
+      return this.todoList
+    },
+    addTodo(todo) {
+      this.todoList.push(todo)
+    },
+    deleteTodo(id) {
+      this.todoList = this.todoList.filter(todo => todo.id !== id)
+    }
+  }
+})
