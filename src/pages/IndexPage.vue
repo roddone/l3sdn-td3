b42da03e25c796bce02f8ac7b528101d0e1f1b09
Sepande <sepande.nivandy@gmail.com>
2024-03-06 13:56:18 +0100

    t

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="q-pa-md">
-    <q-form v-model="formValid" @submit="addTask">
+    <q-form v-model="formValid" >
       <q-input v-model="newTask.name" label="Nom de la tâche" filled dense :rules="[val => !!val || 'Ce champ est requis']"></q-input>
       <q-input v-model="newTask.details" label="Détails de la tâche" filled dense :rules="[val => !!val || 'Ce champ est requis']"></q-input>
       <q-toggle v-model="newTask.completed" label="Terminée" dense></q-toggle> 
-      <q-btn class="q-mt-md" color="primary" label="Ajouter" :disable="!formValid"></q-btn> 
+      <q-btn class="q-mt-md" color="primary" label="Ajouter" :disable="!isInputValid" @click="addTask" ></q-btn> 
     </q-form>
-
     <q-list bordered class="q-mt-md">
       <q-linear-progress stripe size="10px" :value="completedTasksPercentage" color="primary" class="q-mb-md"></q-linear-progress>
       <div class="text-completed-tasks">{{ completedTasksCount }}/{{ tasks.length }} tâches complétées</div>
@@ -21,7 +20,7 @@
             <q-badge v-if="task.completed" color="green" label="Terminée"></q-badge>
             <q-badge v-else color="red" label="Non terminée"></q-badge>
           </div>
-          <q-banner class="bg-primary text-white" color="primary" label="Détails" clickable> Détails </q-banner>
+          <q-banner class="bg-primary text-white" color="primary" label="Détails" clickable > Détails </q-banner>
           <q-collapse v-model="task.showDetails">
             <q-card>
               <q-card-section>
@@ -48,8 +47,6 @@ const tasks = ref([
   { name: 'Tâche 3', details: 'Détails de la tâche 3', completed: false, showDetails: true }
 ])
 
-const formValid = ref(false)
-
 const addTask = () => {
   if (newTask.value.name.trim() !== '' || newTask.value.details.trim() !== '') {
     tasks.value.push({
@@ -59,7 +56,6 @@ const addTask = () => {
       showDetails: true
     })
     newTask.value = { name: '', details: '', completed: false } 
-    formValid.value = false
   }
 }
 
@@ -75,6 +71,9 @@ const completedTasksPercentage = computed(() => {
   return (completedTasksCount.value / tasks.value.length)
 })
 
+const isInputValid = computed(() => {
+  return newTask.value.name.trim() !== '' && newTask.value.details.trim() !== ''
+})
 </script>
 
 <style>
